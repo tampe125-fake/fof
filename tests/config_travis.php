9b3f7880a5c16513c52b727fc07c47921b663168
tampe125 <tampe125@gmail.com>
2013-08-08 12:01:50 +0200

    working on paths

--- tests/config_travis.php
+++ tests/config_travis.php
@@ -9,8 +9,8 @@
  */
 
 $fofTestConfig = array(
-	'2.5' => realpath('environments/2.5/unzip'),
-	'3.x' => realpath('environments/3.x/unzip'),
+	'2.5' => realpath(__DIR__.'/environments/2.5/unzip'),
+	'3.x' => realpath(__DIR__.'environments/3.x/unzip'),
 	'dbparams' => array(
 		'host'  => '127.0.0.1',
 		'user'  => 'travis',
